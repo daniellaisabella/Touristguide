f0ffa17e4f51fa35896f3e2385c3bcee838daf5b
chrisHS03 <chso0005@stud.kea.dk>
2025-02-28 14:27:51 +0100

    styling

--- src/test/java/org/example/touristguide/controller/TouristControllerTest.java
+++ src/test/java/org/example/touristguide/controller/TouristControllerTest.java
@@ -1,63 +1,51 @@
 package org.example.touristguide.controller;
 
 import org.example.touristguide.service.TouristService;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @WebMvcTest(TouristController.class)
-
 class TouristControllerTest {
 
-   @Autowired
-   private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-   @MockitoBean
-   private TouristService touristService;
+    @MockitoBean
+    private TouristService touristService;
 
     @BeforeEach
     void setUp() {
     }
 
-    @Test
-    void getAttractions() {
+    @AfterEach
+    void tearDown() {
     }
 
     @Test
-    void showTags() {
+    void getAttractions() throws Exception {
+        mockMvc.perform(get("/attractions"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("attraction-list"));
     }
 
     @Test
-    void addAttraction() throws Exception {
-        mockMvc.perform(post("/attractions/add")
-                .param("attraction name", "attraction value")
-                .param("another attraction name", "another attraction value")
-                .param("last attraction name", "last attraction value"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/attractions"));
+    void showTags() throws Exception{
 
-    }
+        String name = "Tivoli";
 
-    @Test
-    void getAttractionsByName() {
-    }
-
-    @Test
-    void editAttraction() {
-    }
-
-    @Test
-    void updateAttraction() {
-    }
-
-    @Test
-    void deleteAttraction() {
+        mockMvc.perform(get("/{name}/tags", name))
+                .andExpect(status().isOk())
+                .andExpect(view().name("tags"));
     }
 }
